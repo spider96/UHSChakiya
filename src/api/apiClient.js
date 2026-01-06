a3f1a706070228bb978e24ed1afdf6ff3048bc5e
uhschakiya <crc.uhschakiya@gmail.com>
2026-01-06 11:31:25 +0530

    added condiotional url for testing

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { getToken } from '../utils/storage';
 
 const apiClient = axios.create({
-  baseURL: 'http://10.0.2.2:8080/api', // change if needed
+  //baseURL: 'http://10.0.2.2:8080/api', // change if needed
+  baseURL: 'http://192.168.31.227:8080/api', // change if needed
   headers: {
     'Content-Type': 'application/json',
   },
